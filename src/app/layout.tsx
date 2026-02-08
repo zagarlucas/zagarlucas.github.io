609e1fb100255c1983b93b8c1b2dfff068555ac4
lucas <muzeeh7@gmail.com>
2026-02-08 15:04:03 -0300

    CV_v1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -28,6 +29,19 @@ export default function RootLayout({
                     crossOrigin="anonymous"
                     referrerPolicy="no-referrer"
                 />
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-NRRR9PM4D8"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+
+                        gtag('config', 'G-NRRR9PM4D8');
+                    `}
+                </Script>
             </head>
             <body className={inter.className}>{children}</body>
         </html>
